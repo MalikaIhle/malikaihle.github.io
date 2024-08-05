3ceccbfd98a426185b20505e8f4b90d8b41debe1
Patrick Callahan <patricktcallahan18@gmail.com>
2024-08-05 19:58:40 +0200

    Create script for mass generating needed html redirects

---
+++ mass_generate_files.R
@@ -0,0 +1,32 @@
+
+
+
+# https://lmu-osc.github.io/Simulations-for-Advanced-Power-Analyses/ does not have a sitemap.xml file
+
+links <- c(
+  "https://lmu-osc.github.io/Collaborative-RStudio-GitHub/sitemap.xml",
+  "https://lmu-osc.github.io/Introduction-RStudio-Git-GitHub/sitemap.xml",
+  "https://lmu-osc.github.io/Introduction-Simulations-in-R/sitemap.xml",
+  "https://lmu-osc.github.io/OSC-Open-Research-Summer-School-2023/sitemap.xml"
+) %>%
+  lapply(function(x) get_links_from_sitemaps(x))
+
+links_vec <- links %>% 
+  unlist() %>% 
+  unname() %>%
+  str_remove_all("https://lmu-osc.github.io/") 
+
+
+parsed_table <- links_vec %>%
+  tibble(link = .) %>%
+  mutate(
+    old_repo = stringr::str_extract(link,  "^[^/]*/") %>%
+      stringr::str_remove("/"),
+    page = stringr::str_remove(link,  "^[^/]*/") 
+    ) 
+
+purrr::map2(
+  parsed_table$old_repo,
+  parsed_table$page,
+  ~ create_redirect_file(old_repo = .x, page = .y)
+)
